d6b1437ec2f95dd9f94b5b62109869ddf2133e74
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-10 16:51:59 +0700

    createnavbar and setting player starts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: %i[home hiscores]
+  before_action :set_stats, only: %i[game]
   def home
   end
 
@@ -6,5 +8,21 @@ class PagesController < ApplicationController
   end
 
   def hiscores
+    @users = User.order('wins DESC')
+  end
+
+  def played
+    current_user.wins += 1 if params[:win]
+    current_user.losses += 1 if params[:loss]
+    current_user.ties += 1 if params[:tie]
+    current_user.save
+  end
+
+  private
+
+  def set_stats
+    @wins = current_user.wins
+    @losses = current_user.losses
+    @ties = current_user.ties
   end
 end
